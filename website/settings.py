3758445132006cc3d3b13be476604ed4e95c515c
Al Fatih <abdurrahmanalfatih72@gmail.com>
2019-11-01 18:03:22 +0700

    Try to deploy

--- website/settings.py
+++ website/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS = (
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
